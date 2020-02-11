7ec564354b006e43aaefc509a132f5234e32a7fa
writeens <44687555+writeens@users.noreply.github.com>
2020-02-11 10:22:40 +0100

    Fixed about us modal

---
+++ About Us/index.js
@@ -0,0 +1,33 @@
+const meetModal = document.querySelector('#meetModal')
+const meetModalClose = document.querySelector('#meetModalClose');
+const meetTeamItems = document.querySelectorAll('.meet-team-item');
+const meetModalName = document.querySelector('#meet-modal-name');
+const meetModalDesc = document.querySelector('.meet-modal-desc')
+const meetModalContentRight = document.querySelector('.meet-modal-content-right')
+const meetModalTitle = document.querySelector('#meet-modal-title');
+const meetModalImage = document.querySelector('#meet-modal-image');
+const handleTeamMemberClick = (elem) => {
+    let id = elem.getAttribute('data-team-member-id')
+    console.log(id)
+    let data = TEAM[id]
+    const {imageLocation, name, title, paragraph} = data
+    meetModalName.innerHTML = name;
+    meetModalTitle.innerHTML = title;
+    meetModalImage.src = imageLocation;
+    meetModalDesc.innerHTML = ""
+    paragraph.map(item => {
+        let para = document.createElement('p');
+        para.setAttribute('class', 'meet-paragraph')
+        para.innerHTML = item
+        meetModalDesc.append(para)
+    })
+    meetModal.style.display = 'flex';
+}
+meetTeamItems.forEach(item => {
+    item.addEventListener('click', () => {handleTeamMemberClick(item)})
+})
+
+//Close Modal
+meetModalClose.addEventListener('click', () => {
+    meetModal.style.display = 'none';
+})
---
+++ About Us/meetInfo.js
@@ -0,0 +1,41 @@
+const TEAM = {
+    0:{
+        imageLocation:'../Assets/Debayo-Doherty.jpg',
+        name:'OLA DEBAYO-DOHERTY',
+        title:'Group Chief Executive Officer (C.E.O)',
+        paragraph:['Ola Debayo-Doherty joined Mandilas in 2018 as the Group Chief Executive Officer charged with leading the company through its restructuring exercise. She is the first African to attain the position since the Group started operations 70 years ago.',
+            'Ola joined the Mandilas Group from IBILE Oil and Gas Corporation where she was the pioneer Managing Director/Chief Executive Officer. Prior to this role, she was at Shell Petroleum Development Company of Nigeria Limited, Shell International, United Kingdom and Arthur Anderson where she served in Finance, Management Consulting, Strategic Business Development and Project/Process Management functions at senior and management levels in and outside Nigeria.',
+            'She is a strong program and project management professional with a Master of Business Administration (M.B.A) from Canisius College (USA) and Bachelor of Science (B.S.) Accounting degree from D’Youville College (USA).']
+    },
+    1:{
+        imageLocation:'../Assets/Daisi.jpg',
+        name:'OMIDIJI DAISI',
+        title:'COMPANY SECRETARY / GROUP FINANCIAL CONTROLLER',
+        paragraph:['Daisi is a professional accountant with over 20 years’ experience across the broad spectrum of corporate finance. \
+                He was Company Treasurer and rose to Head of Corporate Planning and Strategy at Promasidor Nigeria and later joined Timeless Holdings Ltd as the Chief Operating Officer. \
+                Prior to this, he had worked with MBC International Bank', 'He holds a B.Sc. degree from the Obafemi Awolowo University and an MBA from the University of Benin. He is a Fellow of the Institute of Chartered Accountants of Nigeria and the Chartered Institute of Taxation of Nigeria. \
+                He’s also a project manager – a PRINCE 2 Registered Practitioner and an Executive Master’s Certificate holder in Project Management from the Project Management College, UK. \
+                A member of many professional bodies both in Nigeria and UK']
+    },
+    2:{
+        imageLocation:'../Assets/Pal.jpg',
+        name:'PAL SINGH',
+        title:'General Manager of the Air-Conditioning Division',
+        paragraph:['Pal Singh is the General Manager of the Air-conditioning division. Qualified as HVACR engineer, he is a member of the American Society of Heating, Refrigeration and Air-conditioning Engineers. Pal has 28 years’ experience in large Air-conditioning and ventilation equipment from different Companies across the globe. He is an expert in latest and advanced technology of HVACR engineering which is a unique service that aligns comfort and technology for Corporates.']
+    },
+    3:{
+        imageLocation:'../Assets/Koyejo.jpg',
+        name:'KEMI KOYEJO',
+        title:'General Manager of Motors / Leasing Division',
+        paragraph:['Kemi holds a Bachelor of Arts degree and an MBA from the University of Ife and Obafemi Awolowo University respectively. She worked briefly at the then University of Ife before proceeding to join the Federal Road Safety Commission where she had a brief stint as Route Commander, Public Enlightenment.',
+        'In 1991, she joined Mandilas Enterprises Limited as Public Enlightenment Officer, later became the Business Development Manager and eventually National Marketing Manager. Kemi’s automobile industry experience spans over 25years. She is a member of the Nigerian Institute of Management, as well as the Automobile and Allied Industries Sector of the Lagos Chamber of Commerce and has won several industry awards including Toyota Nigeria. She left Mandilas in 2010 and re-joined the team in 2018 as Operations Manager of Motors/Leasing Division.']
+    },
+    4:{
+        imageLocation:'../Assets/Ernest.jpg',
+        name:'ERNEST ONYENZE',
+        title:'Head, Group Property',
+        paragraph:['Ernest Onyenze is the Head, Group Property. He holds a B.Sc degree in Estate Management and a Masters’ degree in Facilities Management from the University of Lagos. He also holds a diploma in Building technology.',
+        'A registered Estate Surveyor and Valuer, and an associate of the Nigerian Institution of Estate Surveyors and Valuers (NIESV). He is a member of International Facilities Management Association (IFMA) Nigerian Chapter.',
+        'He has had varied experiences in real estate practice spanning close to three decades. Worked at different times with Ministry of Works and Transport, Bauchi, Bauchi State, Adeniji Osunkojo Associates – a firm of Estate Surveyors and Valuers. He joined Mandilas Group Limited Property Division in April 1, 1993 as an Estate Surveyor and rose to the position of Head, Group Property.']
+    }
+}
