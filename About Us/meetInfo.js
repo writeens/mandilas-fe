7c4b8e2915691278cfb8e38b2c24e30ef160773f
writeens <44687555+writeens@users.noreply.github.com>
2020-03-04 09:04:42 +0100

    added promise mantra section to homepage

--- About Us/meetInfo.js
+++ About Us/meetInfo.js
@@ -37,5 +37,33 @@ const TEAM = {
         paragraph:['Ernest Onyenze is the Head, Group Property. He holds a B.Sc degree in Estate Management and a Masters’ degree in Facilities Management from the University of Lagos. He also holds a diploma in Building technology.',
         'A registered Estate Surveyor and Valuer, and an associate of the Nigerian Institution of Estate Surveyors and Valuers (NIESV). He is a member of International Facilities Management Association (IFMA) Nigerian Chapter.',
         'He has had varied experiences in real estate practice spanning close to three decades. Worked at different times with Ministry of Works and Transport, Bauchi, Bauchi State, Adeniji Osunkojo Associates – a firm of Estate Surveyors and Valuers. He joined Mandilas Group Limited Property Division in April 1, 1993 as an Estate Surveyor and rose to the position of Head, Group Property.']
-    }
+    },
+    5:{
+        imageLocation:'https://res.cloudinary.com/mandilas/image/upload/v1582705380/Assets/Debayo-Doherty_sb9l9d.png',
+        name:'OLA DEBAYO-DOHERTY',
+        title:'Group Chief Executive Officer (C.E.O)',
+        paragraph:['Ola Debayo-Doherty joined Mandilas in 2018 as the Group Chief Executive Officer charged with leading the company through its restructuring exercise. She is the first African to attain the position since the Group started operations 70 years ago.',
+            'Ola joined the Mandilas Group from IBILE Oil and Gas Corporation where she was the pioneer Managing Director/Chief Executive Officer. Prior to this role, she was at Shell Petroleum Development Company of Nigeria Limited, Shell International, United Kingdom and Arthur Anderson where she served in Finance, Management Consulting, Strategic Business Development and Project/Process Management functions at senior and management levels in and outside Nigeria.',
+            'She is a strong program and project management professional with a Master of Business Administration (M.B.A) from Canisius College (USA) and Bachelor of Science (B.S.) Accounting degree from D’Youville College (USA).']
+    },
+    6:{
+        imageLocation:'https://res.cloudinary.com/mandilas/image/upload/v1582705379/Assets/Daisi_zftpca.png',
+        name:'OMIDIJI DAISI',
+        title:'COMPANY SECRETARY / GROUP FINANCIAL CONTROLLER',
+        paragraph:['Daisi is a professional accountant with over 20 years’ experience across the broad spectrum of corporate finance. \
+                He was Company Treasurer and rose to Head of Corporate Planning and Strategy at Promasidor Nigeria and later joined Timeless Holdings Ltd as the Chief Operating Officer. \
+                Prior to this, he had worked with MBC International Bank', 'He holds a B.Sc. degree from the Obafemi Awolowo University and an MBA from the University of Benin. He is a Fellow of the Institute of Chartered Accountants of Nigeria and the Chartered Institute of Taxation of Nigeria. \
+                He’s also a project manager – a PRINCE 2 Registered Practitioner and an Executive Master’s Certificate holder in Project Management from the Project Management College, UK. \
+                A member of many professional bodies both in Nigeria and UK']
+    },
+    7:{
+        imageLocation:'https://res.cloudinary.com/mandilas/image/upload/v1582705379/Assets/Daisi_zftpca.png',
+        name:'OMIDIJI DAISI',
+        title:'COMPANY SECRETARY / GROUP FINANCIAL CONTROLLER',
+        paragraph:['Daisi is a professional accountant with over 20 years’ experience across the broad spectrum of corporate finance. \
+                He was Company Treasurer and rose to Head of Corporate Planning and Strategy at Promasidor Nigeria and later joined Timeless Holdings Ltd as the Chief Operating Officer. \
+                Prior to this, he had worked with MBC International Bank', 'He holds a B.Sc. degree from the Obafemi Awolowo University and an MBA from the University of Benin. He is a Fellow of the Institute of Chartered Accountants of Nigeria and the Chartered Institute of Taxation of Nigeria. \
+                He’s also a project manager – a PRINCE 2 Registered Practitioner and an Executive Master’s Certificate holder in Project Management from the Project Management College, UK. \
+                A member of many professional bodies both in Nigeria and UK']
+    },
 }
