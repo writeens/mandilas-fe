d57ad3c3a5f36e6712e20eb659615260ef0a5960
writeens <44687555+writeens@users.noreply.github.com>
2020-03-10 16:12:37 +0100

    setup camry page

--- Motors/motorInfo.js
+++ Motors/motorInfo.js
@@ -191,5 +191,69 @@ const MOTORS = {
             28:["DAYTIME RUNNING LIGHT", "LED", "", "", ""],
             29:["WIRELESS DOOR LOCK", "Smart Entry", "", "", ""]
         }
+    },
+    3:{
+        name:"Toyota Camry",
+        overview:{
+            motorStructure:"Sedan",
+            engineFuelType:"Petrol",
+            motorType:"Automatic",
+        },
+        images:{
+            main:"https://res.cloudinary.com/mandilas/image/upload/v1583852474/Vehicles/Camry/toyota_camry_00-798x466_p5wlfh.jpg",
+            interior:{
+                0:"https://res.cloudinary.com/mandilas/image/upload/v1583852478/Vehicles/Camry/Interior09-798x466_c4xeig.jpg",
+                1:"https://res.cloudinary.com/mandilas/image/upload/v1583852477/Vehicles/Camry/Interior04-798x466_sggovk.jpg",
+                2:"https://res.cloudinary.com/mandilas/image/upload/v1583852477/Vehicles/Camry/Interior03-1023x687_qqhsh8.jpg",
+                3:"https://res.cloudinary.com/mandilas/image/upload/v1583852477/Vehicles/Camry/Interior07-798x466_uacwtg.jpg",
+                4:"https://res.cloudinary.com/mandilas/image/upload/v1583852476/Vehicles/Camry/Interior02-1023x687_nfyq4u.jpg"
+            },
+            exterior:{
+                0:"https://res.cloudinary.com/mandilas/image/upload/v1583852425/Vehicles/Camry/Toyota-Website-11.resized-798x466_em9f6v.jpg",
+                1:"https://res.cloudinary.com/mandilas/image/upload/v1583852424/Vehicles/Camry/Toyota-Website-5.resized-798x466_ukoryy.jpg",
+                2:"https://res.cloudinary.com/mandilas/image/upload/v1583852423/Vehicles/Camry/Toyota-Website-3.resized-798x466_lwqyna.jpg",
+                3:"https://res.cloudinary.com/mandilas/image/upload/v1583852427/Vehicles/Camry/Exterior18-798x466_skfijl.jpg"
+            }
+        },
+        summary:[
+            "Super intelligent 4-speed automatic transmission.",
+            "Optimum Response, Maximum Efficiency and Auto air conditioning system.",
+            "Multi-information display",
+            "Electric power steering system"
+        ],
+        colors:["#a33638", "#9dbfcb", "#4dae74", "#242424", "#c9a267"],
+        description:"The styling of the Avensis gives a dynamic and unique presence. The outstanding basic perfomance delivers agile driving with a reassuring feeling. Your pleasure is enhanced by the spacious cabin with its premium-quality interior finish and satisfying equipment. Creating a new dynamism full of originality, the Avensis provides a cutting-edge at the top level in its class.",
+        specSheet:{
+            0:["", "Model", "", "2.5 Litre AT", "3.5 Litre AT"],
+            1:["Major Specifications", "Description", "", "&#10004;", "&#10004;"],
+            2:["ENGINE", "engine", "(2.0L) 3ZR-FE", "", ""],
+            3:["", "Engine Type", "4Cyl 16-Valve in line with Twin Cam VVT-i", "", ""],
+            4:["TRANSMISSION","4 Speed Automatic", "", "", ""],
+            5:["SUSPENSION", "Front", "MacPherson Struts", "", ""],
+            6:["", "Rear", "Double Wishbone", "", ""],
+            7:["BRAKES", "Front", "Ventilated Discs", "", ""],
+            8:["", "Rear", "Discs", "", ""],
+            9:["FUEL SYSTEM", "Electronic Fuel Injection (EFI)", "", "", ""],
+            10:["FUEL TANK CAPACITY", "60 Litres", "", "", ""],
+            11:["TYRE & WHEEL", "205/60R15", "", "", ""],
+            12:["SEAT MATERIAL", "Fabric", "", "", "&#10004;"],
+            13:["", "Leather", "", "&#10004;", ""],
+            14:["SEAT CAPACITY", "5 Persons", "", "", ""],
+            15:["OUTSIDE REAR VIEW MIRROR", "Color+Power+Retractable+ Heater", "", "", ""],
+            16:["SEAT HEATER", "D+P(Dial Switch)", "", "&#10004;", ""],
+            17:["FRONT SEAT ADJUSTER", "Manual (D 6Way, P 4Way)", "", "", ""],
+            18:["AUDIO", "RD+CD+BT 6 Speaker", "", "", "&#10004;"],
+            19:["", "8 Inches Display Audio 6Speakers", "", "&#10004;", ""],
+            20:["HEAD LAMP", "Halogen+DRL(LED)", "", "", "&#10004;"],
+            21:["", "LED+DRL(Light Guide)", "", "&#10004;", ""],
+            22:["FRONT FOG & DRIVING LAMP", "Standard", "", "", ""],
+            23:["", "Standard with Cornering Lamp", "", "&#10004;", ""],
+            24:["HILL START ASSIST SYSTEM", "", "", "&#10004;", ""],
+            25:["PISTON DISPLACEMENT", "1986cc", "", "", ""],
+            26:["IGNITION", "Push Start", "", "", ""],
+            27:["STEERING SWITCH", "Tel+2Way Audio+BT", "", "", ""],
+            28:["DAYTIME RUNNING LIGHT", "LED", "", "", ""],
+            29:["WIRELESS DOOR LOCK", "Smart Entry", "", "", ""]
+        }
     }
 }
