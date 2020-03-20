4a298a8979eca157f2bfc9ec3f749eddc73b20de
writeens <44687555+writeens@users.noreply.github.com>
2020-03-20 17:04:30 +0100

    setup service booking

--- public/Airconditioners After Sales/index.js
+++ public/Airconditioners After Sales/index.js
@@ -11,3 +11,20 @@ const handleCloseAsMobilityModal = () => {
     asMobilityModal.style.display = "none"
 }
 closeAsMobilityModal.addEventListener('click', handleCloseAsMobilityModal)
+
+
+// Handle Service Booking
+const goToServiceBooking = document.querySelector('#goToServiceBooking');
+
+const handleServiceBooking = () => {
+    let token = localStorage.getItem('mandilasToken');
+    console.log(token)
+    token = JSON.parse(token)
+    if((token === 'undefined') || (token === "")){
+        asMobilityModal.style.display = "none";
+        loginModal.style.display = "flex"
+    }else{
+        location.href = '../Account Profile/index.html';
+    }
+}
+goToServiceBooking.addEventListener('click', handleServiceBooking)
