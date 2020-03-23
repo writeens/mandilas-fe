fe4c833d3c689d2ea3940e4ed003c934e5cb9101
writeens <44687555+writeens@users.noreply.github.com>
2020-03-23 15:01:20 +0100

    fixed used cars modal setup

--- public/Motors/index.js
+++ public/Motors/index.js
@@ -3,6 +3,66 @@
 const motorsButtons = document.querySelectorAll('.motors-buttons > button');
 const oldVehicles = document.querySelector('.old-vehicles');
 const newVehicles = document.querySelector('.new-vehicles');
+const closeUsedCarsModal = document.querySelector('#closeUsedCarsModal');
+const usedCarsModal = document.querySelector('#usedCarsModal');
+const usedModalName = document.querySelector('#usedModalName')
+const usedModalSummary = document.querySelector('#usedModalSummary')
+const usedModalImage = document.querySelector('#usedModalImage');
+const usedModalPrice = document.querySelector('#usedModalPrice');
+const usedModalMileage = document.querySelector('#usedModalMileage');
+const usedModalColor = document.querySelector('#usedModalColor');
+
+
+
+//Generate Used Vehicle
+const generateUsedVehicle = (data) => {
+    let newData = Object.values(data)
+    newData.map(item => {
+        createVehicleItem(item)
+    })
+}
+
+
+/**Used Car Modal Control */
+const populateModal = (data) => {
+    const {year, make, model, summary, image, color, mileage, price} = data;
+    usedModalImage.src = image;
+    usedModalName.innerHTML = `${make} ${model} ${year}`;
+    usedModalPrice.innerHTML = formatter.format(price);
+    usedModalSummary.innerHTML = summary.join(', ');
+    usedCarsModal.style.display = 'flex';
+    usedModalMileage.innerHTML = mileage;
+    usedModalColor.innerHTML = color
+}
+/**Used Car Modal Control */
+
+/**Close Modal */
+closeUsedCarsModal.addEventListener('click', () => {
+    usedCarsModal.style.display = "none";
+})
+/**Close Modal */
+
+
+const usedCarsContainer = document.querySelector('#usedCarsContainer');
+usedCarsContainer.innerHTML = "";
+const createVehicleItem = (vehicle) => {
+    const {year, make, model, summary, image, color, mileage, price} = vehicle
+    let itemContainer = document.createElement('div');
+    itemContainer.classList.add('motors-uv-item');
+    let itemImage = document.createElement('img');
+    itemImage.src = image;
+    let title = document.createElement('p');
+    title.innerHTML = `${make} ${model} ${year}`;
+    let itemPrice = document.createElement('p')
+    itemPrice.innerHTML = formatter.format(price);
+
+    itemContainer.addEventListener('click',() => populateModal(vehicle));
+
+    itemContainer.append(itemImage, title, itemPrice);
+    usedCarsContainer.append(itemContainer)
+}
+
+
 //Handle Motors Buttons Click
 let vehicleState = 'new'
 const handleMotorsButtons = (item, index, arr) => {
@@ -23,12 +83,15 @@ const handleMotorsButtons = (item, index, arr) => {
             vehicleState = 'used'
             oldVehicles.style.display = "flex"
             newVehicles.style.display = "none"
+            generateUsedVehicle(USED_CARS);
         }
     }
 }
 motorsButtons.forEach((item, index, arr) => {
     item.addEventListener('click', () => {handleMotorsButtons(item, index, arr)})
 })
+
+
 
 const handleMotorsLoad = () => {
     let params = new URLSearchParams(window.location.search.substring(1));
@@ -37,6 +100,7 @@ const handleMotorsLoad = () => {
         newVehicles.style.display = "none";
         oldVehicles.style.display = "flex";
         vehicleState = "used"
+        // generateUsedVehicle();
         motorsButtons[0].classList.toggle('motors-button-active');
         motorsButtons[1].classList.toggle('motors-button-active');
     }
@@ -71,3 +135,4 @@ window.addEventListener('load', () => {
     }, 5000);
 })
 /**Celebration Modal Control */
+
---
+++ public/Motors/used.js
@@ -0,0 +1,102 @@
+const USED_CARS = {
+    0:{
+        year: 2016,
+        make:"Toyota",
+        model:"Hilux",
+        summary:["Manual", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"White",
+        mileage:110400,
+        price:12300000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    1:{
+        year: 2016,
+        make:"Toyota",
+        model:"Hilux",
+        summary:["Manual", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"White",
+        mileage:47000,
+        price:12300000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    2:{
+        year: 2016,
+        make:"Honda",
+        model:"CR-V",
+        summary:["Auto", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"Blue",
+        mileage:9878,
+        price:13000000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    3:{
+        year: 2014,
+        make:"Honda",
+        model:"CR-V",
+        summary:["Auto", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"Grey",
+        mileage:43102,
+        price:10000000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    4:{
+        year: 2012,
+        make:"Toyota",
+        model:"Hilux",
+        summary:["Manual", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"Grey",
+        mileage:136664,
+        price:9700000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    5:{
+        year: 2009,
+        make:"Hyundai",
+        model:"Santa-Fe",
+        summary:["Manual", "CD", "AC", "L/S", "Imported", "Airbag", "Very Neat"],
+        color:"Blue",
+        mileage:138028,
+        price:3300000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    6:{
+        year: 2007,
+        make:"Toyota",
+        model:"Yaris",
+        summary:["Auto", "CD", "AC", "F/S", "Imported", "Airbag", "Very Neat"],
+        color:"Red",
+        mileage:43785,
+        price:2700000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    7:{
+        year: 2010,
+        make:"4Runner",
+        model:"Hilux",
+        summary:["Auto", "CD", "AC", "F/S", "Imported", "Airbag", "Registered"],
+        color:"Grey",
+        mileage:144665,
+        price:5000000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    8:{
+        year: 2012,
+        make:"Toyota",
+        model:"Corolla",
+        summary:["Auto", "CD", "AC", "F/S", "Imported", "Airbag"],
+        color:"Black",
+        mileage:83568,
+        price:3800000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    },
+    4:{
+        year: 2014,
+        make:"Ford",
+        model:"Focus",
+        summary:["Manual", "CD", "AC", "F/S", "Imported", "Airbag", "Registered"],
+        color:"Black",
+        mileage:27885,
+        price:1900000,
+        image:"https://res.cloudinary.com/mandilas/image/upload/v1583918567/Vehicles/Hilux/14bb0a_4a545b0502fd4c17bc9b6854d601a8fb_mv2-1024x683_cy820p.jpg"
+    }
+}
